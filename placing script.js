022d6b5d433ce80c276d261b2a84d6bf2fdfd4fd
ESWAR0044 <150889483+ESWAR0044@users.noreply.github.com>
2023-11-20 22:19:15 +0530

    Create placing script.js
    alert("Hello,World");

---
+++ placing script.js
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+ <title>
+    JAVASCRIPT
+ </title>          
+
+
+
+</head>
+<body>
+<h1>JAVASCRIPT for Beginners</h1>
+<div id="content">
+
+    <p> blah blash blash </p>
+</div>
+
+    <script src="script.js">
+
+    </script>
+</body>
+
+</html>
